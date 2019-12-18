ec78fac1ae1fc95f0d3780009507c705cc1f94d7
Armando Lopez <mandosteel.06@gmail.com>
2019-12-18 10:07:16 -0700

    Create shipbull.h

---
+++ shipbull.h
@@ -0,0 +1,21 @@
+//#pragma once
+////missile class
+//class shipbull {
+//private:
+//	double xPos;
+//	double yPos;
+//	double angle;
+//	double radius;
+//	bool isAlive = 0;
+//public:
+//	shipbull(int x, int y);
+//	void move();
+//	void draw();
+//	void kill();
+//	int getX();
+//	int getY();
+//	bool isalive();
+//	bool offScreen();
+//	void shoot(int x, int y);
+//	bool collide(double x, double y);
+//};
