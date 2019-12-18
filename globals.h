8680d852f0bf9a6ab7121622fbc880a821ee48ba
Armando Lopez <mandosteel.06@gmail.com>
2019-12-18 10:10:57 -0700

    Create globals.h

---
+++ globals.h
@@ -0,0 +1,10 @@
+#pragma once
+//constants: variables that shouldn't change once the game starts
+const float FPS = 60;
+const int SCREEN_W = 800;
+const int SCREEN_H = 800;
+//enumeration to help you remember what numbers represent which directions
+enum MYKEYS {
+	KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT, KEY_SPACE
+};
+//int i = 0;
